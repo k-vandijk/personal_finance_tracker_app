5885f963b780eac2316e39a857cb91ff80e2d094
Kevin van Dijk <kevinvd05@gmail.com>
2025-03-11 18:24:17 +0100

    Added: Token saving to local storage

--- financeapp_app/lib/services/auth_service.dart
+++ financeapp_app/lib/services/auth_service.dart
@@ -4,12 +4,36 @@ import 'package:financeapp_app/dtos/authentication_dto.dart';
 import 'package:financeapp_app/dtos/response_dto.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService extends ChangeNotifier {
   String? _token;
 
   bool get isAuthenticated => _token != null;
   String? get token => _token;
+
+  AuthService() {
+    _loadTokenFromStorage();
+  }
+
+  /// Loads the token from local storage when the service is initialized.
+  Future<void> _loadTokenFromStorage() async {
+    final prefs = await SharedPreferences.getInstance();
+    _token = prefs.getString('jwt_token');
+    notifyListeners();
+  }
+
+  /// Saves the token to local storage.
+  Future<void> _saveTokenToStorage(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('jwt_token', token);
+  }
+
+  /// Removes the token from local storage.
+  Future<void> _removeTokenFromStorage() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('jwt_token');
+  }
 
   /// Private helper method to perform an authentication POST request.
   Future<Response> _authRequest(String endpoint, AuthenticationDTO dto) async {
@@ -23,8 +47,9 @@ class AuthService extends ChangeNotifier {
 
       if (response.statusCode == 200) {
         _token = response.body;
+        await _saveTokenToStorage(_token!);
         notifyListeners();
-        return Response(message: 'Successfully logged in.', success: true);
+        return Response(message: '', success: true);
       } 
     
       final errorMessage = 'Failed to $endpoint: ${response.body}';
@@ -33,6 +58,7 @@ class AuthService extends ChangeNotifier {
     
     catch (e) {
       final errorMessage = 'An error occurred while trying to $endpoint: $e';
+      print(errorMessage);
       return Response(message: errorMessage, success: false);
     }
   }
@@ -45,8 +71,9 @@ class AuthService extends ChangeNotifier {
     return await _authRequest('register', dto);
   }
 
-  void logout() {
+  Future<void> logout() async {
     _token = null;
+    await _removeTokenFromStorage();
     notifyListeners();
   }
 }
